dc52b9e51b1c9c9038c1fe753b4c8e56bfacdf64
Alisson Mayer Medeji <alissonmmedeji@gmail.com>
2024-07-09 17:03:52 -0300

    Initial commit

---
+++ src/main/java/alisson/consumindo_APIs_Externas/ConsumingExternalApIsApplication.java
@@ -0,0 +1,13 @@
+package alisson.consumindo_APIs_Externas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConsumingExternalApIsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConsumingExternalApIsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/alisson/consumindo_APIs_Externas/ConsumingExternalApIsApplicationTests.java
@@ -0,0 +1,13 @@
+package alisson.consumindo_APIs_Externas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConsumingExternalApIsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
