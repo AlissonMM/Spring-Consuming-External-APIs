f6484b7e5ce0ffd30aa720739f7e976dd4a23c55
Alisson Mayer Medeji <alissonmmedeji@gmail.com>
2024-07-10 18:52:01 -0300

    Finish ViaCepService

--- src/main/java/alisson/consumindo_APIs_Externas/service/ViaCepService.java
+++ src/main/java/alisson/consumindo_APIs_Externas/service/ViaCepService.java
@@ -1,8 +1,18 @@
 package alisson.consumindo_APIs_Externas.service;
 
+import alisson.consumindo_APIs_Externas.model.Address;
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @FeignClient(name = "viacep", url = "https://viacep.com.br/ws")
 public interface ViaCepService {
-    //need to finish
+  //One way to do it ->
+  // @RequestMapping(method = RequestMethod.GET, value = "/{cep}/json/")
+
+    //Better way ->
+    @GetMapping("/{cep}/json/")
+    Address consultarCep(@PathVariable("cep") String cep);
 }
